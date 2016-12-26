ebce61b9dce9d6ec913b92788f573a2870ad263e
thaogiang.90@gmail.com <thaogiang.90@gmail.com>
2016-12-26 07:35:37 +0700

    A

--- real_estate/homepage.php
+++ real_estate/homepage.php
@@ -1,7 +1,44 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ThaoGiang
- * Date: 12/22/2016
- * Time: 10:49 PM
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<style>
+    #container {
+        width: 1366px;
+        height: auto;
+        border: 1px red;
+    }
+    #header {
+        width: 1362px;
+        height: 40px;
+        border: 1px yellow;
+        background: yellow;
+    }
+    #navigation {
+        margin-top: 1px;
+        width: 1362px;
+        background: pink;
+        height: 40px;
+    }
+</style>
+<body>
+<div id="container">
+    <div id="header">
+
+
+    </div>
+    <div id="navigation">
+
+    </div>
+    <div id="content">
+
+    </div>
+    <div id="footer">
+
+    </div>
+
+</div>
+</body>
+</html>
--- real_estate/toppage.php
+++ real_estate/toppage.php
@@ -3,12 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    <link rel="stylesheet" type="text/css" href="../css/mystyle.css">
 </head>
 <body>
-<div class="container">
-    <div class="toppage">
-        <img src="">
-    </div>
-</div>
+<div id="toppage">
+<div id="contact">
+    <a>Contact us : 0905303269</a>
+</div><!-- end contact -->
+    <div id="Login_Frame">
+
+    </div><!-- end login -->
+    <div class="LOGIN">
+        <a href="#">Login</a>
+
+    </div><!-- end text login -->
+    <div class="REGISTER">
+        <a href="#">Register</a>
+    </div><!-- end text register -->
+    <div style="clear: both"></div>
+    <div id="CompanyName">
+        <a>AAAAAAA</a>
+
+    </div><!--end toppage -->
+</div> <!-- end toppage -->
 </body>
 </html>
