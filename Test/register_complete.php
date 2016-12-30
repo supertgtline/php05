766d369f93a8d177041a641820f1a8554a053969
thaogiang.90@gmail.com <thaogiang.90@gmail.com>
2016-12-30 07:41:47 +0700

    commit 3012

--- Test/register_complete.php
+++ Test/register_complete.php
@@ -49,7 +49,44 @@ if(check_empty($first_name)){
     echo "Please insert first name";
     exit();
 }
-$last_name = $_REQUEST("last_name");
+$last_name = $_REQUEST["last_name"];
 
-?>
+$username = $_REQUEST["username"];
 
+if(check_username($username)){
+    echo "This username is exist, Please please input other username";
+    exit();
+}
+$password =encrypt_password($_REQUEST["password"]);
+$avatar= $_FILES["avatar"]["name"];
+$target_dir="uploads/";
+$target_file = $target_dir.basename($_FILES["avatar"]["name"]);
+$check = getimagesize($_FILES["avatar"]["tmp_name"]);
+if ($check !== false) {
+    move_uploaded_file($_FILES["avatar"]["tmp_name"],$target_file);
+} else {
+    echo "file is not an image";
+}
+
+$birthday= strtotime($_REQUEST["birthday"]);
+
+if(check_birthday($birthday)){
+    echo "Please insert birthday";
+    exit();
+}
+
+$gender= $_REQUEST["gender"];
+
+$created= strtotime(date('Y/m/d h:i:s'));
+if (empty($gender)) {
+    $gender=1;
+}
+
+$email= $_REQUEST["email"];
+
+$phone= $_REQUEST["phone"];
+
+$sql = "INSERT INTO users (first_name, last_name, username, password, avatar, birthday, gender, email, phone,created,modified) VALUES ('$first_name','$last_name', '$username', '$password', '$avatar', '$birthday', '$gender', '$email', '$phone', '$created', '$created')";
+if ($conn->query($sql)===TRUE) {
+    echo "Register success <a href='login.php'> click here</a> to login";
+}
